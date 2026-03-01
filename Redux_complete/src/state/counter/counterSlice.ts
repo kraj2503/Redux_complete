d37f120049d8299e0f142addaea79a49a6df51d5
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2026-03-02 01:53:34 +0530

    redux-store

---
+++ Redux_complete/src/state/counter/counterSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+    reducers: {
+        increment: (state,action) => {
+            state.value+=1
+      }
+  },
+});
+
+export default counterSlice.reducer;
